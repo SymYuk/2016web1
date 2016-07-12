018ebb74c977ffe4898a7bb62a6e259101d19c0f
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-07-12 12:36:26 +0900

    Update chase.js

--- chase.js
+++ chase.js
@@ -1,23 +1,26 @@
-var	scrollTop=$(window).scrollTop(),
-	headerH=$('#header').outerHeight(true),
-	subH=$('aside').outerHeight(true),
-	wrapperH=$('#wrapper').outerHeight(true);
+$(function()
+{
+	var	scrollTop=$(window).scrollTop(),
+		headerH=$('#header').outerHeight(true),
+		subH=$('#sub').outerHeight(true),
+		wrapperH=$('#wrapper').outerHeight(true);
 	
 	if(scrollTop<headerH)
 	{
-		$('aside').removeClass('fixed');
-		$('aside').addClass('top');
+		$('#sub').removeClass('fixed');
+		$('#sub').addClass('top');
 	}
 	
 	else if(scrollTop+subH<wrapperH)
 	{
-		$('aside').removeClass('top');
-		$('aside').removeClass('bottom');
-		$('aside').addClass('fixed');	
+		$('#sub').removeClass('top');
+		$('#sub').removeClass('bottom');
+		$('#sub').addClass('fixed');	
 	}
 	
 	else
 	{
-		$('aside').removeClass('fixed');
-		$('aside').addClass('bottom');	
+		$('#sub').removeClass('fixed');
+		$('#sub').addClass('bottom');	
 	}
+});
